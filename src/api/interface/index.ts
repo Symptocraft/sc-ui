3225f1ad53af8c0511d358c140923814bf75f1aa
Aaron Wallace <arttheft.situation@gmail.com>
2024-01-23 13:36:00 -0500

    Add Custom Axios Error Handling

--- src/api/interface/index.ts
+++ src/api/interface/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { ApiOptions } from "../../types/api/interface.type";
+import AxiosErrorHandler from "./errors";
 
 const APIInterface = async (options: ApiOptions) => {
     const { url, method, data, headers, params, withCredentials } = options;
@@ -17,6 +18,11 @@ const APIInterface = async (options: ApiOptions) => {
     if(data){
         api.defaults.data = data
     }
+
+    api.interceptors.response.use(
+        (response) => response,
+        AxiosErrorHandler
+    )
 
     return api;
 }
